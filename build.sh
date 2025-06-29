f558c075ef0016fc6484c48a36cad9cd3b24f749
Nick Schneble <nschneble@users.noreply.github.com>
2025-06-29 14:23:14 +0100

    add pangram api

--- build.sh
+++ build.sh
@@ -6,11 +6,17 @@ cp robots.txt public/robots.txt
 cp sitemap.xml public/sitemap.xml
 
 # create the html folder structure
+mkdir public/pangram
 cp html/index.html public/index.html
+cp html/pangram.html public/pangram/index.html
 
 # compile css
 mkdir public/css
 sass css/style.scss public/css/style.css
+
+# copy data files
+mkdir public/data
+cp data/* public/data
 
 # copy images and favicons
 mkdir public/images
